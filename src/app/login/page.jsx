6bf7c10da4518a863aa71d98253b84a79e85ca4d
Adriana Jacobo <adriana.jacobo@catolica.edu.sv>
2024-06-04 00:36:36 -0600

    Initial commit

---
+++ src/app/login/page.jsx
@@ -0,0 +1,11 @@
+import { Container, Form } from "react-bootstrap";
+import Image from "next/image";
+import logo from "@/assets/images/logoletra.png"
+
+export default function Login(){
+    return(
+        <Container fluid>
+             <h1>prueba</h1>
+        </Container>
+    );
+}
